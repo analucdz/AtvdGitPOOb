b864f9d0fd649e4ed042c02282307f73762fddb4
Samuel de Andrade <samuelvasc11@gmail.com>
2025-06-05 22:29:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    ArrayList<Pessoa> pessoas = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("""
+                MENU:
+                1) Adicionar aluno:
+                2) Adicionar professor: 
+                3) Exibir dados de aluno: 
+                4) Exibir dados de professor: 
+                """);
+        System.out.println("Digite a opção: ");
+        int opcao = input.nextInt();
+
+    switch(opcao){
+        case 1:
+
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        default:
+        break;
+
+
+
+    }
+}}
